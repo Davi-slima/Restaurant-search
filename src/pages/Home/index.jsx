39955543a216db13f44b39755438b81ce5141b31
Davi dos Santos <davi.slima75@gmail.com>
2021-09-01 16:58:06 -0300

    Development Carousel and image card components

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,25 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { useState } from 'react';
 import TextField, { Input } from '@material/react-text-field';
 import MaterialIcon from '@material/react-material-icon';
 
 import logo from '../../assets/logo.svg';
+import restaurante from '../../assets/restaurante-fake.png';
+import { Card } from '../../components';
 
-import { Container, Search, Logo, Wrapper, Map } from './styles';
+import { Container, Search, Logo, Wrapper, Map, CarouselTitle, Carousel } from './styles';
 
 const Home = () => {
-
     const [inputValue, setInputValue] = useState('');
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        adaptiveHeight: true,
+    };
 
     return (
         <Wrapper>
@@ -23,6 +34,17 @@ const Home = () => {
                             value={inputValue}
                             onChange={(e) => setInputValue(e.target.value)} />
                     </TextField>
+                    <CarouselTitle>Na sua Área</CarouselTitle>
+                    <Carousel {...settings}>
+                        <Card photo = {restaurante} title = "Restaurante" />
+                        <Card photo = {restaurante} title = "Restaurante" />
+                        <Card photo = {restaurante} title = "Restaurante" />
+                        <Card photo = {restaurante} title = "Restaurante" />
+                        <Card photo = {restaurante} title = "Restaurante" />
+                        <Card photo = {restaurante} title = "Restaurante" />
+                        <Card photo = {restaurante} title = "Restaurante" />
+                        <Card photo = {restaurante} title = "Restaurante" />
+                    </Carousel>
                 </Search>
             </Container>
             <Map />
